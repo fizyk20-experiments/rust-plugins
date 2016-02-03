cd2fab08bf257f3cfab3bf7e5db3b57fa6fdbbd2
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-02-03 10:26:09 +0100

    Fixed tabs/spaces problems

--- plugin_test/src/main.rs
+++ plugin_test/src/main.rs
@@ -6,22 +6,22 @@ use std::env::current_dir;
 
 fn main() {
     let mut path = current_dir().unwrap();
-	path.push("libplugin.so");
-	println!("Path: {}", path.display());
+    path.push("libplugin.so");
+    println!("Path: {}", path.display());
 
     let lib = Library::new(path.as_path()).unwrap();
 
-	println!("Success.");
+    println!("Success.");
 
-	let object_factory: Symbol<extern fn() -> Box<PluginTrait>> = unsafe {
+    let object_factory: Symbol<extern fn() -> Box<PluginTrait>> = unsafe {
         lib.get(b"object_factory").unwrap()
-	};
+    };
 
-	println!("Success 2.");
+    println!("Success 2.");
 
-	let obj = object_factory();
+    let obj = object_factory();
 
-	println!("Success 3.");
+    println!("Success 3.");
 
-	println!("Result: {}", obj.get_some_string());
+    println!("Result: {}", obj.get_some_string());
 }
